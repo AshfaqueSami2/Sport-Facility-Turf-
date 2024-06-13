698fe427129f599cfa33173ed621ef8a96c958e1
AshfaqueSami2 <ashfaquesami2@gmail.com>
2024-06-13 19:43:42 +0600

    project setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
+
 
 const app: Application = express();
 
@@ -8,8 +8,6 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-//application routes
-app.use('/api/v1/students',StudentRoutes)
 
 const getAController = (req: Request, res: Response) => {
   const a = 10;
